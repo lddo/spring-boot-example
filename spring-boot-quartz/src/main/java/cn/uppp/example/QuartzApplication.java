900c484ee80d524b534af56ca5d73c74587f1fa8
ldd <admin@uppp.cn>
2019-04-04 19:28:49 +0800

    spring-boot-quartz

---
+++ spring-boot-quartz/src/main/java/cn/uppp/example/QuartzApplication.java
@@ -0,0 +1,19 @@
+package cn.uppp.example;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 应用入口
+ * @MapperScan 扫描mapper文件对应接口
+ */
+@SpringBootApplication
+@MapperScan(basePackages = "cn.uppp.example.dao")
+public class QuartzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuartzApplication.class, args);
+    }
+
+}
